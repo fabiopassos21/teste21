0089911283c11d8dabf7497d7ed89599b8bd2f52
fabiopassos21 <fabiopassos21>
2022-05-12 11:42:56 -0300

    Inserção de Scanner

---
+++ Seção4/Aula26.java
@@ -0,0 +1,23 @@
+package Seção4;
+
+import java.util.Scanner;
+
+public class Aula26 {
+	public static void main(String []args) {
+
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Digite um valor ");
+		String x = sc.next();
+		System.out.println(x);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
---
+++ Seção4/Aula26_PT3.java
@@ -0,0 +1,24 @@
+package Seção4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula26_PT3 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Double x;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um valor Double");
+		x = sc.nextDouble();
+		System.out.println(x);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
