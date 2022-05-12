5884b38dede620e9792f678bc87b1591760b0ed4
fabiopassos21 <fabiopassos21>
2022-05-12 10:08:52 -0300

    Segundo

---
+++ Seção4/Aula25.java
@@ -0,0 +1,16 @@
+package Seção4;
+
+public class Aula25 {
+	public static void main (String []args) {
+		int x = 5;
+		double y = 2*x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		
+		
+		
+	}
+
+}
