4935c8c24f96050baec79896c776a235589c0fc6
fabiopassos21 <fabiopassos21>
2022-05-12 13:12:27 -0300

    ultima aula

---
+++ Seção4/Aula27.java
@@ -0,0 +1,21 @@
+package Seção4;
+
+import java.util.Scanner;
+
+public class Aula27 {
+
+	public static void main(String[] args) {
+		String x;
+		Scanner sc = new Scanner (System.in);
+		String s1, s2, s3;
+		System.out.println("Digite algo");
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+		System.out.println("DADOS DIGITADOS:");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+	}
+
+}
