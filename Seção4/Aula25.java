250ea1c2990f0199c6ff6ce6853b5866208c3331
fabiopassos21 <fabiopassos21>
2022-05-12 11:40:27 -0300

    Aulas sobre Scanner

--- Seção4/Aula25.java
+++ Seção4/Aula25.java
@@ -2,13 +2,12 @@ package Seção4;
 
 public class Aula25 {
 	public static void main (String []args) {
-		int x = 5;
-		double y = 2*x;
-		
-		System.out.println(x);
-		System.out.println(y);
-		
-		
+		double b,B,h,area;
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		area = (b* + B)/2.0 * h;
+		System.out.println(area);
 		
 		
 	}
